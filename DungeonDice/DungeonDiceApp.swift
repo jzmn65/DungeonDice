87b167203a2d8e81c1cba7ef6eaf51a9766cefdb
Jazmine Singh <jzmn65@gmail.com>
2025-09-27 09:25:39 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Jazmine Singh on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
